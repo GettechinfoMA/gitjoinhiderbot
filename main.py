da6ec2f6042bd18e09ef16a8efccdd4a97c2a9ba
MA <60564425+GettechinfoMA@users.noreply.github.com>
2021-01-11 16:28:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,159 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# pylint: disable=W0613, C0116
-# type: ignore[union-attr]
-# This program is dedicated to the public domain under the CC0 license.
+from os import environ
+# Moved Back to asyncio-dev branch of pyrogram
+from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
+import pyshorteners
 
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY')
 
-import logging
-from typing import Dict
-
-from telegram import ReplyKeyboardMarkup, Update
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    ConversationHandler,
-    CallbackContext,
-)
-
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
-
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
-
-reply_keyboard = [
-    ['Age', 'Favourite colour'],
-    ['Number of siblings', 'Something else...'],
-    ['Done'],
-]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+bot = Client('Shortlink bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
 
 
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    facts = list()
-
-    for key, value in user_data.items():
-        facts.append(f'{key} - {value}')
-
-    return "\n".join(facts).join(['\n', '\n'])
+@bot.on_message(Filters.command('start') & Filters.private)
+async def start(bot, update):
+    await update.reply(
+        f"**Hi {update.chat.first_name}!**\n\n"
+        "I'm shortlink bot. Just send me link and get adsless short link")
 
 
-def start(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(
-        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
-        "Why don't you tell me something about yourself?",
-        reply_markup=markup,
-    )
+@bot.on_message(Filters.regex(r'https?://[^\s]+') & Filters.private)
+async def link_handler(bot, update):
+    link = update.matches[0].group(0)
+    if API_KEY:
+      try:
+        s = pyshorteners.Shortener(api_key=API_KEY) 
+        shortened_url = s.cuttly.short(link)
+        button = [[InlineKeyboardButton("Link 🔗", url=shortened_url)]]
+        markup = InlineKeyboardMarkup(button)
+        await update.reply_text(text=f'Here is your shortlink \n`{shortened_url}`', reply_markup=markup, quote=True)
+        
+      except Exception as e:
+        await update.reply(f'Error: {e}', quote=True)
+    else:
+      try:
+        s = pyshorteners.Shortener() 
+        shortened_url = s.dagd.short(link)
+        button = [[InlineKeyboardButton("Link 🔗", url=shortened_url)]]
+        markup = InlineKeyboardMarkup(button)
+        await update.reply_text(text=f'Here is your shortlink \n`{shortened_url}`', reply_markup=markup, quote=True)
+        
+      except Exception as e:
+        await update.reply(f'Error: {e}', quote=True)
 
-    return CHOOSING
+      
 
-
-def regular_choice(update: Update, context: CallbackContext) -> int:
-    text = update.message.text
-    context.user_data['choice'] = text
-    update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
-
-    return TYPING_REPLY
-
-
-def custom_choice(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(
-        'Alright, please send me the category first, ' 'for example "Most impressive skill"'
-    )
-
-    return TYPING_CHOICE
-
-
-def received_information(update: Update, context: CallbackContext) -> int:
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
-
-    update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
-        " on something.",
-        reply_markup=markup,
-    )
-
-    return CHOOSING
-
-
-def done(update: Update, context: CallbackContext) -> int:
-    user_data = context.user_data
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)} Until next time!"
-    )
-
-    user_data.clear()
-    return ConversationHandler.END
-
-
-def main() -> None:
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("1230921328:AAHJd3AyhU1nAfBlJHDZLAO431ND1qLm8Zk", use_context=True)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            CHOOSING: [
-                MessageHandler(
-                    Filters.regex('^(Age|Favourite colour|Number of siblings)$'), regular_choice
-                ),
-                MessageHandler(Filters.regex('^Something else...$'), custom_choice),
-            ],
-            TYPING_CHOICE: [
-                MessageHandler(
-                    Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice
-                )
-            ],
-            TYPING_REPLY: [
-                MessageHandler(
-                    Filters.text & ~(Filters.command | Filters.regex('^Done$')),
-                    received_information,
-                )
-            ],
-        },
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
-    )
-
-    dispatcher.add_handler(conv_handler)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+bot.run()
